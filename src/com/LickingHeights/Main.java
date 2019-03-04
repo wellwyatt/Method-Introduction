b7878803bc06baf01d5e1be856b2cbbfc62b7797
wyatt <wyatt.schlicher.2021@student.lhschools.org>
2019-03-04 07:54:32 -0500

    Finished the "Hello World" and user Methods

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,13 +1,30 @@
 package com.LickingHeights;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        String userName;
+       Scanner keyboard;
+        keyboard = new Scanner(System.in);
+       Question();
+       userName = keyboard.nextLine();
+       printGreetings(userName);
 
 
 
 
-        
     }
+    public static void printGreetings (String userName){
+    System.out.println("Hello "+ userName);
+
+    }
+   public static void Question(){
+
+        System.out.println("What is your name?");
+
+   }
+   //public static void
+
 }
