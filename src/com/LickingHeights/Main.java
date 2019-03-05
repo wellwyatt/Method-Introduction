ff1f65bb13f0139e68a05679b3600c17f2276365
wyatt <wyatt.schlicher.2021@student.lhschools.org>
2019-03-05 07:44:58 -0500

    Added multiplication and division methods

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,30 +1,48 @@
 package com.LickingHeights;
+
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         String userName;
-       Scanner keyboard;
+        Scanner keyboard;
         keyboard = new Scanner(System.in);
-       Question();
-       userName = keyboard.nextLine();
-       printGreetings(userName);
+        double number1, number2;
+        Question1();
+        userName = keyboard.nextLine();
+        printGreetings(userName);
+        System.out.println("Give me a number. ");
+        number1 = keyboard.nextInt();
+        System.out.println("Give me another number.");
+        number2 = keyboard.nextInt();
+        System.out.println(division(number1, number2));
+        System.out.println(multiplication(number1,number2));
 
+    }
 
+    public static void printGreetings(String userName) {
+        System.out.println("Hello " + userName);
 
 
     }
-    public static void printGreetings (String userName){
-    System.out.println("Hello "+ userName);
 
-    }
-   public static void Question(){
-
+    public static void Question1() {
         System.out.println("What is your name?");
+    }
 
-   }
-   //public static void
+
+    public static double division(double numberA, double numberB) {
+        return numberA / numberB;
+    }
+    public static double multiplication(double numberC, double numberD){
+
+
+        return numberC*numberD;
+    }
 
 }
+
+
+
