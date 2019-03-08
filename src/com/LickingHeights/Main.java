539eb877c17d25a71d2927788d986e8fbd8d0a6d
wyatt <wyatt.schlicher.2021@student.lhschools.org>
2019-03-08 07:46:03 -0500

    Finished my methods project.

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,7 +1,7 @@
 package com.LickingHeights;
 
 import java.util.Scanner;
-
+import java.lang.Math;
 public class Main {
 
     public static void main(String[] args) {
@@ -10,6 +10,7 @@ public class Main {
         Scanner keyboard;
         keyboard = new Scanner(System.in);
         double number1, number2;
+        helloworld();
         Question1();
         userName = keyboard.nextLine();
         printGreetings(userName);
@@ -25,7 +26,15 @@ public class Main {
         System.out.println("Give me another");
         wordB = keyboard.nextLine();
         System.out.println(wordFlip(wordA, wordB));
+        System.out.println("The Max Number is " +maxNum(number1,number2));
+
     }
+
+    public static void helloworld() {
+        System.out.println("Hello World");
+
+    }
+
 
     public static void printGreetings(String userName) {
         System.out.println("Hello " + userName);
@@ -51,10 +60,17 @@ public class Main {
     public static String wordFlip(String wordA, String wordB) {
         System.out.println("The words flipped are ");
         return (wordB + " " + wordA);
+    }
+
+    public static double maxNum(double numberA, double numberB) {
+        if (numberA > numberB) {
+            return numberA;
+        } else if (numberA < numberB) {
+            return numberB;
+        } else
+            System.out.println("The numbers are equal.");
+            return numberA + numberB;
+        }
 
 
     }
-}
-
-
-
